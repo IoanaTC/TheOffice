78fae4f3fed027a2d5a58289cf028c3ae239fb46
Melania Ion <melaniaion24@gmail.com>
2022-12-09 13:11:03 +0200

    +modificarea in ApplicationDbContext de care uitasem

--- TheOffice/Data/ApplicationDbContext.cs
+++ TheOffice/Data/ApplicationDbContext.cs
@@ -1,13 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TheOffice.Models;
+using Task = TheOffice.Models.Task;
 
 namespace TheOffice.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+        public DbSet<Project> Projects;
+        public DbSet<Comment> Comments;
+        public DbSet<Task> Tasks;
+
     }
 }
