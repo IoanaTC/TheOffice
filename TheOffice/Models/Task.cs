1adeeb023c5b403646a99dc5533bc640ed3b7a09
Melania Ion <melaniaion24@gmail.com>
2022-12-09 12:57:13 +0200

    Comment, Task, Project Models added

---
+++ TheOffice/Models/Task.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheOffice.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titlul este obligatoriu")]
+        [MinLength(5, ErrorMessage = "Titlul trebuie sa aiba mai mult de 5 caractere")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Descrierea task-ului este obligatorie")]
+        public string Content { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime Deadline { get; set; }
+        public string Status { get; set; }
+
+        //-- atachement
+
+        public int? ProjectId { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+
+        public virtual Project? Project { get; set; }
+
+        public virtual ICollection<Comment>? Comments { get; set; }
+
+    }
+}
