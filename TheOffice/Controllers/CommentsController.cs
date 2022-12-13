ebc9d2ae9ecd2e3f89247ab032c14dff5a38baa3
Melania Ion <melaniaion24@gmail.com>
2022-12-13 10:56:44 +0200

    Revert "Revert "Adaugare SeedData + Service Provider""
    This reverts commit 65d47e440cc27c4fced5c3d2b163eed368911f5f.

---
+++ TheOffice/Controllers/CommentsController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using TheOffice.Data;
+using TheOffice.Models;
+
+namespace TheOffice.Controllers
+{
+    [Authorize]
+    public class CommentsController : Controller
+    {
+        private readonly ApplicationDbContext db;
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public CommentsController(
+            ApplicationDbContext context,
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
+        {
+            db = context;
+
+            _userManager = userManager;
+
+            _roleManager = roleManager;
+        }
+
+        // Stergerea unui comentariu asociat unui task existent in baza de date
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Delete(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                db.Comments.Remove(comm);
+                db.SaveChanges();
+                return Redirect("/Tasks/Show/" + comm.TaskId);
+            }
+
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul de a sterge comentariul";
+                return RedirectToAction("Index", "Tasks");
+            }
+        }
+
+        // Se editeaza un comentariu existent(intr-o pagina separata)
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Edit(int id)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                return View(comm);
+            }
+
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa editati acest comentariu";
+                return RedirectToAction("Index", "Tasks");
+            }
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "User,Admin")]
+        public IActionResult Edit(int id, Comment requestComment)
+        {
+            Comment comm = db.Comments.Find(id);
+
+            if (comm.UserId == _userManager.GetUserId(User) || User.IsInRole("Admin"))
+            {
+                if (ModelState.IsValid)
+                {
+                    comm.Content = requestComment.Content;
+
+                    db.SaveChanges();
+
+                    return Redirect("/Tasks/Show/" + comm.TaskId);
+                }
+                else
+                {
+                    return View(requestComment);
+                }
+            }
+            else
+            {
+                TempData["message"] = "Nu aveti dreptul sa faceti modificari";
+                return RedirectToAction("Index", "Tasks");
+            }
+        }
+    }
+}
